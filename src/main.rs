4fabbe3906b1dc649a9e9534b72faf1349520593
Tahmid Sadik <tahmidsadik112@gmail.com>
2019-03-31 03:52:21 +0600

    Upgrades ggez to .5.rc-1.0

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
 extern crate ggez;
 extern crate rand;
 use ggez::event::{self, MouseButton};
-use ggez::graphics::{self, Image, Point2};
+use ggez::graphics::{self, Image};
+use ggez::mint::{Vector2, Point2};
 use ggez::*;
 
 struct WindowState {
-    width: u32,
-    height: u32,
+    width: f32,
+    height: f32,
 }
 
 struct Velocity {
@@ -34,8 +35,8 @@ struct Circle {
 }
 
 static window: WindowState = WindowState {
-    width: 1024,
-    height: 768,
+    width: 640.0,
+    height: 480.0,
 };
 
 struct MainState {
@@ -65,18 +66,16 @@ impl MainState {
     }
 }
 
-fn world_to_screen_coords(screen_width: u32, screen_height: u32, point: Point2) -> Point2 {
-    let width = screen_width as f32;
-    let height = screen_height as f32;
-    let x = point.x + width / 2.0;
-    let y = height - (point.y + height / 2.0);
-    Point2::new(x, y)
+fn world_to_screen_coords(screen_width: f32, screen_height: f32, point: Point2<f32>) -> Point2<f32> {
+    let x = point.x + screen_width / 2.0;
+    let y = screen_height - (point.y + screen_height / 2.0);
+    Point2{x: x, y: y}
 }
 
 impl event::EventHandler for MainState {
     fn update(&mut self, _ctx: &mut Context) -> GameResult<()> {
         let inc = 0.015;
-        if (self.velocity.y + inc < self.velocity.max_velocity) {
+        if self.velocity.y + inc < self.velocity.max_velocity {
             self.velocity.y = self.velocity.y + inc;
         }
         self.y = self.y - self.velocity.y; // 0.15 is the acceleration
@@ -84,25 +83,25 @@ impl event::EventHandler for MainState {
     }
 
     fn draw(&mut self, ctx: &mut Context) -> GameResult<()> {
-        graphics::clear(ctx);
-        let pos = world_to_screen_coords(window.width, window.height, Point2::new(self.x, self.y));
+        graphics::clear(ctx, [0.0, 0.5, 0.0, 1.0].into());
+        let pos = world_to_screen_coords(window.width, window.height, Point2{x: self.x, y: self.y});
 
-        let drawParams = graphics::DrawParam {
+        let draw_params = graphics::DrawParam {
             dest: pos,
             rotation: 0.0,
-            scale: Point2::new(0.25, 0.25),
-            offset: Point2::new(0.5, 0.5),
+            scale: Vector2{x: 0.25, y: 0.25},
+            offset: Point2{x: 0.5, y: 0.5},
             ..Default::default()
         };
-        graphics::draw_ex(ctx, &self.football, drawParams);
+        graphics::draw(ctx, &self.football, draw_params);
         //        graphics::draw(ctx, &self.football, Point2::new((window.width / 2) as f32, (window.height / 2) as f32), 0.0);
         graphics::present(ctx);
         Ok(())
     }
 
-    fn mouse_button_down_event(&mut self, _ctx: &mut Context, button: MouseButton, x: i32, y: i32) {
+    fn mouse_button_down_event(&mut self, _ctx: &mut Context, button: MouseButton, x: f32, y: f32) {
         self.mouse_down = true;
-        if (button == MouseButton::Left) {
+        if button == MouseButton::Left {
             self.velocity.y = self.velocity.y - 5.0;
         }
         println!(
@@ -112,25 +111,31 @@ impl event::EventHandler for MainState {
     }
 }
 
-pub fn main() {
+pub fn main() -> GameResult {
     let c = conf::Conf {
         window_mode: conf::WindowMode {
             width: window.width,
             height: window.height,
             borderless: false,
-            fullscreen_type: conf::FullscreenType::Off,
-            vsync: true,
-            min_width: 0,
-            max_width: 0,
-            min_height: 0,
-            max_height: 0,
+            fullscreen_type: conf::FullscreenType::Desktop,
+            min_width: 0.0,
+            max_width: 0.0,
+            min_height: 0.0,
+            max_height: 0.0,
+            hidpi: false,
+            maximized: false,
+            resizable: false,
         },
         window_setup: conf::WindowSetup::default(),
         backend: conf::Backend::OpenGL { major: 3, minor: 2 },
+        modules: conf::ModuleConf::default()
     };
-
-    let mut ctx = &mut Context::load_from_conf("super_simple", "ggez", c).unwrap();
-    let state = &mut MainState::new(ctx).unwrap();
-
-    event::run(ctx, state).unwrap();
+    
+    let cb = ContextBuilder::new("touchball", "ggez")
+    .window_setup(conf::WindowSetup::default().title("juggleball"))
+    .window_mode(conf::WindowMode::default().dimensions(window.width, window.height));
+    
+    let (ctx, events_loop) = &mut cb.build()?;
+    let game = &mut MainState::new(ctx)?;
+    event::run(ctx, events_loop, game)
 }
